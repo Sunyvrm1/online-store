12135c2ff29240672fec05851cf6576078a5a8ee
Sushil Verma <115399549+Sunyvrm1@users.noreply.github.com>
2024-01-30 05:36:09 +0000

    11:06

--- dist/js/wishlist.js
+++ dist/js/wishlist.js
@@ -1,5 +1,31 @@
-const storeId1 = localStorage.getItem("btnClick");
-console.log(storeId1);
+// const storeId1 = localStorage.getItem("btnClick");
+// console.log(storeId1);
+
+// fetch("https://fakestoreapi.com/products")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     const matchingId = data.find((item) => item.id.toString() === storeId1);
+//     if (matchingId) {
+//       console.log(matchingId);
+//       const productTitle = document.querySelector(".productTitle");
+//       const productPrice = document.querySelector(".productPrice");
+//       const productImg = document.getElementById("productImg");
+
+//       productTitle.innerHTML = matchingId.title;
+//       productPrice.innerHTML = `$ ${matchingId.price}`;
+//       productImg.src = matchingId.image;
+//     }
+//   });
+
+// const wishlist = document.querySelector(".wishlist");
+// const deleteItem = document.getElementById("delete");
+
+// deleteItem.addEventListener("click", () => {
+//   wishlist.classList.add("vanishItem");
+// });
+
+const storeIds = JSON.parse(localStorage.getItem("cartItems")) || [];
+console.log(storeIds);
 
 fetch("https://fakestoreapi.com/products")
   .then((res) => res.json())
